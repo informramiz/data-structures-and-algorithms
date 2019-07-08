8118e673297780e1ff60e8638aa93c04bf17ba4a
Ramiz Raja <informramiz@gmail.com>
2019-07-08 16:50:34 +0500

    refactor: Rename file

--- strings/string_reverser.py
+++ strings/string_reverser.py
@@ -1,4 +1,5 @@
-"""if the input is the string "water", then the output should be "retaw"""
+""" -------------String reverser--------------
+if the input is the string "water", then the output should be "retaw"""
 def string_reverser(str):
     end = len(str) - 1
     reversed = ""
@@ -9,6 +10,7 @@ def string_reverser(str):
     return reversed
 
 # Test Cases
+print("----string_reverser-----")
 print ("Pass" if ('retaw' == string_reverser('water')) else "Fail")
 print ("Pass" if ('!noitalupinam gnirts gnicitcarP' == string_reverser('Practicing string manipulation!')) else "Fail")
 print ("Pass" if ('3432 :si edoc esuoh ehT' == string_reverser('The house code is: 2343')) else "Fail")
