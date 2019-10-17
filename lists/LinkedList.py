656a7eea89b52a27ff2703e95db49338be12cbe2
Ramiz Raja <informramiz@gmail.com>
2019-10-18 02:51:03 +0500

    feat: Write method to convert LinkedList to Python list

--- lists/LinkedList.py
+++ lists/LinkedList.py
@@ -15,6 +15,16 @@ class LinkedList:
 
         tail.next = Node(value)
 
+    def to_list(self):
+        #Write function to turn Linked List into Python List
+        list = []
+        tail = self.head
+        while tail:
+            list.append(tail.value)
+            tail = tail.next
+
+        return list
+
 def test_linked_list_basic():
     linked_list = LinkedList()
     linked_list.append(1)
@@ -26,3 +36,13 @@ def test_linked_list_basic():
         print(node.value)
         node = node.next
 test_linked_list_basic()
+
+def test_linked_list_to_python_list():
+    linked_list = LinkedList()
+    linked_list.append(3)
+    linked_list.append(2)
+    linked_list.append(-1)
+    linked_list.append(0.2)
+
+    print ("Pass" if  (linked_list.to_list() == [3, 2, -1, 0.2]) else "Fail")
+test_linked_list_to_python_list()
