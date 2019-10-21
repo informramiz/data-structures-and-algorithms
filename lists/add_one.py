8e47b9240db94b8d056e8f45f497ff6989d43c46
Ramiz Raja <informramiz@gmail.com>
2019-10-21 13:13:42 +0500

    feat: Write efficient version of `add_one` problem

--- lists/add_one.py
+++ lists/add_one.py
@@ -11,10 +11,14 @@ Example 2:
 input = [9, 9, 9]
 output = [1, 0, 0, 0]
 
-Solution:
+Solution-1:
 One way to solve this problem is to convert the input array into a number and then add one to it.
 For example, if we have input = [1, 2, 3], you could solve this problem by creating the number
 123 and then separating the digits of the output number 124.
+
+Solution-2, Efficient:
+Just add 1 the way we humans add 1 to any value. Start from the end value, add 1 to it and then keep carry/borrow
+if needed and keep adding the carry to remaining values
 """
 
 
@@ -55,6 +59,11 @@ test_convert_decimal_to_array()
 
 
 def add_one(array):
+    """
+    One way to solve this problem is to convert the input array into a number and then add one to it.
+    For example, if we have input = [1, 2, 3], you could solve this problem by creating the number
+    123 and then separating the digits of the output number 124.
+    """
     if len(array) == 0:
         return ['1']
 
@@ -63,12 +72,27 @@ def add_one(array):
     output = convert_decimal_to_array(decimal)
     return output
 
+def add_one_efficient(array):
+    """
+    Just add 1 the way we humans add 1 to any value. Start from the end value, add 1 to it and then keep carry/borrow
+    if needed and keep adding the carry to remaining values
+    """
 
-assert (add_one([1, 2, 3]) == [1, 2, 4])
-assert (add_one([1, 2, 9]) == [1, 3, 0])
+    # at start the carry will be 1 as we want to add 1
+    carry = 1
+    # run loop in reverse range [start = n-1, end = -1 (exclusive), step = -1)
+    for i in range(len(array)-1, -1, -1):
+        output = carry + array[i]
+        carry = output // 10
+        array[i] = output % 10
 
+    # at the end if there is still carry then prepend it, example: 99 + 1 = 100, in this carry goes to start
+    if carry > 0:
+        array = [carry] + array
 
-def test_add_one(test_case):
+    return array
+
+def test_add_one(test_case, add_one):
     arr = test_case[0]
     solution = test_case[1]
 
@@ -76,22 +100,27 @@ def test_add_one(test_case):
     assert output == solution, f"output: {output}"
 
 
-# Test case-1
-arr = [0]
-solution = [1]
-test_case = [arr, solution]
-test_add_one(test_case)
+def run_test_cases(add_one):
+    # Test case-1
+    arr = [0]
+    solution = [1]
+    test_case = [arr, solution]
+    test_add_one(test_case, add_one)
+
+    # Test case-2
+    arr = [1, 2, 3]
+    solution = [1, 2, 4]
+    test_case = [arr, solution]
+    test_add_one(test_case, add_one)
+
+    # Test case-3
+    arr = [9, 9, 9]
+    solution = [1, 0, 0, 0]
+    test_case = [arr, solution]
+    test_add_one(test_case, add_one)
 
 
-# Test case-2
-arr = [1, 2, 3]
-solution = [1, 2, 4]
-test_case = [arr, solution]
-test_add_one(test_case)
-
-
-# Test case-3
-arr = [9, 9, 9]
-solution = [1, 0, 0, 0]
-test_case = [arr, solution]
-test_add_one(test_case)
+# run test cases on normal `add_one()`
+run_test_cases(add_one)
+# run test cases on `add_one_efficient()`
+run_test_cases(add_one_efficient)
