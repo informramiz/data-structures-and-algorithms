1ead84d56dc7c3da1a14d9ee9ba4b459f22dcb3c
Ramiz Raja <informramiz@gmail.com>
2019-10-26 17:11:38 +0500

    feat: Complete rehashing

--- hashmaps/hash_map.py
+++ hashmaps/hash_map.py
@@ -18,6 +18,21 @@ Thus, using this algorithm, we can get a corresponding integer value for each st
 
 Note that the array used for this purpose is called a bucket array. It is not a special array. We simply choose to give
 a special name to arrays for this purpose. Each entry in this bucket array is called a bucket and the index in which we store a bucket is called bucket index.
+
+Rehasing:
+
+On average, the distribution of entries is such that if we have n entries and b buckets,
+then each bucket does not have more than n/b key-value pair entries. This number which determines
+the load on our bucket array n/b is known as load factor.
+Generally, we try to keep our load factor around or less than 0.7.
+This essentially means that if we have a bucket array of size 10, then the number of
+key-value pair entries will not be more than 7.
+What happens when we get more entries and the value of our load factor crosses 0.7?
+In that scenario, we must increase the size of our bucket array. Also, we must recalculate the bucket index for each entry in the hashn map.
+
+Note: the hash code for each key present in the bucket array would still be the same. However, because of the compression function, the bucket index will change.
+
+Therefore, we need to rehash all the entries in our hash map. This is known as Rehashing.
 """
 
 
@@ -32,12 +47,16 @@ class MapNode(object):
     def __eq__(self, other):
         return isinstance(other, MapNode) and self.key == other.key
 
+    def __repr__(self):
+        return f"MapNode({self.key}, {self.value})"
+
 
 class HashMap:
     def __init__(self, initial_size = 10):
         self.array = [LinkedList() for _ in range(initial_size)]
         self.entries_count = 0
         self.prime = 37
+        self.load_factor = 0.7
 
     def put(self, key, value):
         bucket_index = self.get_bucket_index(key)
@@ -49,6 +68,14 @@ class HashMap:
             key_node.value = value
 
         self.entries_count += 1
+        current_load_factor = self.entries_count / len(self.array)
+        # we need to make sure our load factor is under self.load_factor
+        if current_load_factor > self.load_factor:
+            # as the load factor has increased so let's increase size of our buckets array
+            self._rehash()
+
+    def __put(self, map_node):
+        self.put(map_node.key, map_node.value)
 
     def get(self, key):
         bucket_index = self.get_bucket_index(key)
@@ -81,6 +108,22 @@ class HashMap:
 
         # compress hash code to bring it in range [0, len(array)-1]
         return hash_code % len(self.array)
+
+    def _rehash(self):
+        """
+        Increases the size of the bucket array and copies the entries from old array to new array
+        by putting each entry in new array by getting a fresh hash code as our hash code compression
+        result will differ because we have increased the array size
+        """
+        # create new array
+        old_array = self.array
+        self.array = [LinkedList() for _ in range(2 * len(self.array))]
+
+        # reset entries count
+        self.entries_count = 0
+        for bucket in old_array:
+            for list_node in bucket:
+                self.__put(list_node)
 
     def size(self):
         return self.entries_count
--- hashmaps/hash_map_tests.py
+++ hashmaps/hash_map_tests.py
@@ -3,13 +3,7 @@ from asserts.asserts import assert_
 
 
 def test_hash_map():
-    hash_map = HashMap()
-
-    #Test Hashing
-    expected = 4
-    input = "abcd"
-    actual = hash_map.get_hash_code(input)
-    assert_(expected, actual)
+    hash_map = HashMap(2)
 
     # Test HashMap get and put
     key = "abcde"
@@ -25,6 +19,11 @@ def test_hash_map():
     hash_map.delete("abcde")
     assert_(0, hash_map.size())
 
+    # Test Rehash
+    hash_map.put("mine", "mine")
+    # this should trigger rehashing
+    hash_map.put("hi", "hi")
+    assert_(2, hash_map.size())
     print("All Tests Passed!")
 
 
